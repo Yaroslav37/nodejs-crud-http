6f791fdff26bbc2d5ef2e4c38a24c90e6e92b2a8
Yaroslav <minenkovyaroslav@mail.ru>
2024-06-20 16:26:44 +0300

    feat: add dotenv

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import http from 'http';
 import { handleRequest } from './routes/index.js';
+import dotenv from 'dotenv';
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   handleRequest(req, res);
